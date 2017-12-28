65202c48fdd381e6558897396fda7488aa1f5f08
Alireza Roshanzamir <a.roshanzamir1996@gmail.com>
2017-12-28 19:14:23 -0100

    Reimplementing previous feed model methods using get_source_feed method.

--- bootcamp/feeds/models.py
+++ bootcamp/feeds/models.py
@@ -26,6 +26,9 @@ class Feed(models.Model):
         ordering = ('-date',)
 
     def __str__(self):
+        if self.is_retweet():
+            return self.get_source_feed().__str__()
+
         return self.post
 
     @staticmethod
@@ -42,9 +45,15 @@ class Feed(models.Model):
         return feeds
 
     def get_comments(self):
+        if self.is_retweet():
+            return self.get_source_feed().get_comments()
+
         return Feed.objects.filter(parent=self).order_by('date')
 
     def calculate_likes(self):
+        if self.is_retweet():
+            return self.get_source_feed().calculate_likes()
+
         likes = Activity.objects.filter(activity_type=Activity.LIKE,
                                         feed=self.pk).count()
         self.likes = likes
@@ -52,6 +61,9 @@ class Feed(models.Model):
         return self.likes
 
     def get_likes(self):
+        if self.is_retweet():
+            return self.get_source_feed().get_likes()
+
         likes = Activity.objects.filter(activity_type=Activity.LIKE,
                                         feed=self.pk)
         return likes
@@ -64,11 +76,17 @@ class Feed(models.Model):
         return likers
 
     def calculate_comments(self):
+        if self.is_retweet():
+            return self.get_source_feed().calculate_comments()
+
         self.comments = Feed.objects.filter(parent=self).count()
         self.save()
         return self.comments
 
     def comment(self, user, post):
+        if self.is_retweet():
+            return self.get_source_feed().comment(user, post)
+
         feed_comment = Feed(user=user, post=post, parent=self)
         feed_comment.save()
         self.comments = Feed.objects.filter(parent=self).count()
@@ -76,7 +94,16 @@ class Feed(models.Model):
         return feed_comment
 
     def linkfy_post(self):
+        if self.is_retweet():
+            return self.get_source_feed().linkfy_post()
+
         return bleach.linkify(escape(self.post))
 
     def is_retweet(self):
         return self.source_feed is not None
+
+    def get_source_feed(self):
+        if self.is_retweet():
+            return self.source_feed.get_source_feed()
+
+        return self
